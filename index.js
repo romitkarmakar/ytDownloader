4911d6ab3dab46053b0c65e550ab73a6e6acd4f6
Romit Karmakar <romitkarmakar@gmail.com>
2020-02-13 11:36:02 +0530

    Added more support

--- index.js
+++ index.js
@@ -24,8 +24,65 @@ http
               var mainjson = qs.parse(body);
               var playerResponse = JSON.parse(mainjson.player_response);
 
+              if (playerResponse.playabilityStatus.status == "OK") {
+                res.writeHead(200, { "Content-Type": "application/json" });
+                res.write(JSON.stringify(playerResponse.streamingData));
+                return res.end();
+              } else {
+                res.writeHead(200, { "Content-Type": "application/json" });
+                res.write(JSON.stringify(playerResponse));
+                return res.end();
+              }
+            });
+          }
+        )
+        .on("error", e => {
+          console.log(e);
+        });
+    } else if (route == "/candownload") {
+      https
+        .get(
+          `https://www.youtube.com/get_video_info?video_id=${q.query.id}`,
+          response => {
+            var body = "";
+            response.on("data", d => {
+              body = body + d;
+            });
+
+            response.on("end", function() {
+              var mainjson = qs.parse(body);
+              var playerResponse = JSON.parse(mainjson.player_response);
+              if (playerResponse.playabilityStatus.status == "OK") {
+                res.writeHead(200, { "Content-Type": "text/plain" });
+                res.write("OK");
+                return res.end();
+              } else {
+                res.writeHead(200, { "Content-Type": "text/plain" });
+                res.write("UNPLAYABLE");
+                return res.end();
+              }
+            });
+          }
+        )
+        .on("error", e => {
+          console.log(e);
+        });
+    } else if (route == "/details") {
+      https
+        .get(
+          `https://www.youtube.com/get_video_info?video_id=${q.query.id}`,
+          response => {
+            var body = "";
+            response.on("data", d => {
+              body = body + d;
+            });
+
+            response.on("end", function() {
+              var mainjson = qs.parse(body);
+              var playerResponse = JSON.parse(mainjson.player_response);
+
               res.writeHead(200, { "Content-Type": "application/json" });
-              res.write(JSON.stringify(playerResponse));
+              res.write(JSON.stringify(playerResponse.videoDetails));
               return res.end();
             });
           }
@@ -35,4 +92,4 @@ http
         });
     }
   })
-  .listen(8080);
+  .listen(PORT);
